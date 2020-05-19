4e447e003364596ea4bbab57705bf8ce2deb99fd
Matt Farncombe <matt@farncombe.co>
2020-05-19 11:33:55 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,6 +12,6 @@ class School
   def add_student(full_name, grade)
     @roster = {:"#{grade}" => [full_name]}
     puts roster
-    @roster[grade] << full_name
+    @roster[:"#{grade}"] << full_name
   end 
 end
